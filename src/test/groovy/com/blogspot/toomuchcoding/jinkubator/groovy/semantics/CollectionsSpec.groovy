2756ea6c6d7e88a20fc78348f4d3690c0ef0415b
Marcin Grzejszczak <marcin.grzejszczak@4finance.com>
2014-11-25 11:07:34 +0100

    Finished examples for collections

--- src/test/groovy/com/blogspot/toomuchcoding/jinkubator/groovy/semantics/CollectionsSpec.groovy
+++ src/test/groovy/com/blogspot/toomuchcoding/jinkubator/groovy/semantics/CollectionsSpec.groovy
@@ -41,4 +41,44 @@ class CollectionsSpec extends Specification {
         then:
             result == null
     }
+
+    def 'should find all elements that are applicable to the given function (filter)'() {
+        given:
+            List<Integer> integers = [1, 2, 3, 4, 5, 6]
+        when:
+            List<Integer> results = integers.findAll { it > 3 }
+        then:
+            results == [4, 5, 6]
+    }
+
+    def 'should execute a function on all elements of a collection (map)'() {
+        given:
+            List<Integer> integers = [1, 2, 3, 4, 5, 6]
+        when:
+            List<Integer> results = integers.collect { it * 2 }
+        then:
+            results == [2, 4, 6, 8, 10 , 12]
+    }
+
+    def 'should merge all elements of the collection into a single result (reduce)'() {
+        given:
+            List<Integer> integers = [1, 2, 3, 4, 5, 6]
+        when:
+            Integer result = integers.inject { Integer accumulator, Integer value -> accumulator + value }
+        then:
+            result == (1 + 2 + 3 + 4 + 5 + 6)
+    }
+
+    def 'should show full filter map reduce approach in Groovy'() {
+        given:
+            List<Integer> integers = [1, 2, 3, 4, 5, 6]
+        when:
+            Integer result = integers.findAll { it < 3 }.
+                                      collect { it * 2 }.
+                                      inject { Integer accumulator, Integer value -> accumulator + value }
+        then:
+            result == 6
+    }
+
+
 }
